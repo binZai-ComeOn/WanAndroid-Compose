4329228112069941b97c0fd58caddf81572cf08a
宾有为 <1647562864@qq.com>
2023-07-24 16:25:41 +0800

    完成搜索界面的ui

--- app/src/main/java/com/binyouwei/wanandroid_compose/ui/page/SearchPage.kt
+++ app/src/main/java/com/binyouwei/wanandroid_compose/ui/page/SearchPage.kt
@@ -1,35 +1,25 @@
 package com.binyouwei.wanandroid_compose.ui.page
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.binyouwei.wanandroid_compose.R
 import com.binyouwei.wanandroid_compose.ui.widget.TopSearchBar
 import com.google.accompanist.flowlayout.FlowRow
-import com.google.android.material.internal.FlowLayout
-import kotlinx.coroutines.launch
 
 class SearchPage : ComponentActivity() {
 
@@ -42,23 +32,52 @@ class SearchPage : ComponentActivity() {
             val drawerState = rememberScaffoldState.drawerState
             val text = remember { mutableStateOf("") }
             Scaffold(topBar = {
-                TopSearchBar(this) {
+                TopSearchBar(this, text) {
 
                 }
             }) {
                 it.calculateBottomPadding()
-                Column(modifier = Modifier.fillMaxWidth()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp, 0.dp)
+                ) {
                     Text(
                         text = stringResource(id = R.string.hot_search),
-                        color = colorResource(id = R.color.theme_pink_color_primary)
+                        color = colorResource(id = R.color.theme_pink_color_primary),
+                        modifier = Modifier.padding(top = 16.dp, bottom = 10.dp)
                     )
                     FlowRow() {
-                        Button(
-                            {}, shape = RoundedCornerShape(10.dp),
+                        TextButton(
+                            onClick = { text.value = "测试" },
+                            contentPadding = PaddingValues(5.dp, 0.dp),
+                            shape = RoundedCornerShape(0.dp)
                         ) {
-                            Text(text = "测试")
+                            Text(
+                                text = "测试", modifier = Modifier
+                                    .background(colorResource(id = R.color.theme_pink_color_primary))
+                                    .padding(10.dp),
+                                color = colorResource(id = R.color.White)
+                            )
+                        }
+                        TextButton(
+                            onClick = { text.value = "测试测试" },
+                            contentPadding = PaddingValues(5.dp, 0.dp),
+                            shape = RoundedCornerShape(0.dp)
+                        ) {
+                            Text(
+                                text = "测试测试", modifier = Modifier
+                                    .background(colorResource(id = R.color.theme_pink_color_primary))
+                                    .padding(10.dp),
+                                color = colorResource(id = R.color.White)
+                            )
                         }
                     }
+                    Text(
+                        text = stringResource(id = R.string.search_history),
+                        color = colorResource(id = R.color.theme_pink_color_primary),
+                        modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
+                    )
                 }
             }
         }
--- app/src/main/java/com/binyouwei/wanandroid_compose/ui/widget/TopNavigationBar.kt
+++ app/src/main/java/com/binyouwei/wanandroid_compose/ui/widget/TopNavigationBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -86,10 +87,10 @@ fun TopSearchMenuBar(
 @Composable
 fun TopSearchBar(
     activity: ComponentActivity,
+    text : MutableState<String>,
     placeholder: String = stringResource(id = R.string.search),
     onClickSearch: (String) -> Unit,
 ) {
-    val text = remember { mutableStateOf("") }
     TopAppBar(
         title = {
             TextField(
