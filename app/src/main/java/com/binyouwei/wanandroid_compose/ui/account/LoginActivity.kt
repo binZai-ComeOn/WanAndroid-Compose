fb0ef84ca06b7927f4f0799cf7439511481f7d44
宾有为 <1647562864@qq.com>
2023-08-02 23:19:18 +0800

    编写登录页代码

--- app/src/main/java/com/binyouwei/wanandroid_compose/ui/account/LoginActivity.kt
+++ app/src/main/java/com/binyouwei/wanandroid_compose/ui/account/LoginActivity.kt
@@ -3,19 +3,23 @@ package com.binyouwei.wanandroid_compose.ui.account
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,6 +41,7 @@ class LoginActivity : AppCompatActivity() {
         login()
     }
 
+    @Preview
     @Composable
     fun login() {
 
@@ -75,20 +80,37 @@ class LoginActivity : AppCompatActivity() {
                 text = account,
                 placeholder = stringResource(id = R.string.please_input_account),
                 leadingIcon = painterResource(id = R.mipmap.ic_account_normal),
-                modifier = Modifier.padding(top = 100.dp)
+                modifier = Modifier
+                    .padding(top = 100.dp)
+                    .background(colorResource(id = R.color.White))
             )
             InputText(
                 text = password,
                 placeholder = stringResource(id = R.string.please_input_password),
                 leadingIcon = painterResource(id = R.mipmap.ic_password_normal),
-                modifier = Modifier.padding(top = 10.dp)
+                keyboardType = KeyboardType.Password,
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .background(color = Color.White)
             )
             Button(
                 onClick = { /*TODO*/ },
-                modifier = Modifier.padding(top = 30.dp)
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .width(200.dp)
+                    .height(44.dp)
+                    .clip(shape = CircleShape),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(id = R.color.Light_Blue),
+                    contentColor = colorResource(
+                        id = R.color.White
+                    )
+                )
             ) {
                 Text(
                     text = stringResource(id = R.string.login),
+                    modifier = Modifier
+                        .background(colorResource(id = R.color.Light_Blue))
                 )
             }
         }
--- app/src/main/java/com/binyouwei/wanandroid_compose/ui/widget/TextField.kt
+++ app/src/main/java/com/binyouwei/wanandroid_compose/ui/widget/TextField.kt
@@ -2,19 +2,21 @@ package com.binyouwei.wanandroid_compose.ui.widget
 
 import android.graphics.drawable.Icon
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import com.binyouwei.wanandroid_compose.R
 
@@ -29,9 +31,15 @@ fun InputText(
     text: MutableState<String>,
     leadingIcon: Painter,
     placeholder: String = "",
+    keyboardType: KeyboardType = KeyboardType.Text,
     modifier: Modifier = Modifier,
+    singleLine: Boolean = true,
     onValueChange: (String) -> Unit = {}
 ) {
+    var visualTransformation = VisualTransformation.None
+    if (keyboardType == KeyboardType.Password) {
+        visualTransformation = PasswordVisualTransformation()
+    }
     TextField(
         value = text.value,
         onValueChange = { newValue ->
@@ -48,8 +56,10 @@ fun InputText(
                     id = R.string.account
                 )
             )
-        }, trailingIcon = {
-            if (text.value.isNotEmpty()){
+        },
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+        trailingIcon = {
+            if (text.value.isNotEmpty()) {
                 IconButton(onClick = { text.value = "" }) {
                     Icon(
                         painter = painterResource(id = R.mipmap.ic_delete),
@@ -59,7 +69,13 @@ fun InputText(
                     )
                 }
             }
-        }, modifier = modifier,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        },
+        singleLine = singleLine,
+        modifier = modifier,
+        visualTransformation = visualTransformation,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = keyboardType
+        ),
     )
 }
